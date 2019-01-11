8cf42399d4066e8b604264451eee8058bac7138d
SinfulS0ul <pasichnikmaks@gmail.com>
2019-01-11 19:23:27 +0200

    'm'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyChlZqdRnYww-ExG3RE2Qjnbc4ITj6El3U',
+    authDomain: 'tic-tac-toe-7f561.firebaseapp.com',
+    databaseURL: 'https://tic-tac-toe-7f561.firebaseio.com',
+    projectId: 'tic-tac-toe-7f561',
+    storageBucket: 'tic-tac-toe-7f561.appspot.com',
+    messagingSenderId: '714701361188',
+  }
 };
