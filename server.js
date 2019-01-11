d5f4c06939eca4704e6614f3d09764c23df7c1be
SinfulS0ul <pasichnikmaks@gmail.com>
2019-01-11 21:01:22 +0200

    'g'

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/Tic-tac-toe'));
+app.use(express.static(__dirname + '/dist/tic-tac-toe'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/Tic-tac-toe/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/tic-tac-toe/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
